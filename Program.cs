431deb82551e109db7e31241e7eb54c306d3ad38
Santosh Rana <mailpysantosh@gmail.com>
2019-11-23 15:26:00 +0545

    struct operator in c#

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 
 namespace Exercise
 {
+	public struct Person{
+		public string fName;
+		public int Age;
+		public int wtg;
+
+	} 
+
     class Program
     {
         static void Main(string[] args)
@@ -10,6 +17,15 @@ namespace Exercise
         	ConstOverLoad con2 = new ConstOverLoad("Santosh");
         	Console.WriteLine(con1.Show());
         	Console.WriteLine(con2.Show());
+        	// For struct
+        	Person p1;
+        	Console.WriteLine("Enter the First Name");
+        	p1.fName = Console.ReadLine();
+        	Console.WriteLine("Enter the Age");
+        	p1.Age = Convert.ToInt32(Console.ReadLine());
+        	Console.WriteLine("Enter the Weight");
+        	p1.wtg = Convert.ToInt32(Console.ReadLine());
+        	Console.WriteLine($"First Name is : {p1.fName}, Age is {p1.Age} and Weigh is : {p1.wtg}");
         }
     }
 }
